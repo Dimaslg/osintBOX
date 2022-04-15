90d4ae6d060fa962ca1b69154d760c8fb543b23b
Dimas <dimas.exe@gmail.com>
2022-04-15 22:32:37 +0200

    Update osintBOX.sh

--- osintBOX.sh
+++ osintBOX.sh
@@ -5,27 +5,27 @@
 mkdir /home/osint/tools
 cd /home/osint/tools
 
-echo
-echo                               _       _   ____   _____  __
-echo                      ___  ___(_)_ __ | |_| __ ) / _ \ \/ /
-echo                     / _ \/ __| | '_ \| __|  _ \| | | \  /
-echo                    | (_) \__ \ | | | | |_| |_) | |_| /  \
-echo                     \___/|___/_|_| |_|\__|____/ \___/_/\_\
-echo
+echo " "
+echo "                              _       _   ____   _____  __    "
+echo "                     ___  ___(_)_ __ | |_| __ ) / _ \ \/ /    "
+echo "                    / _ \/ __| | '_ \| __|  _ \| | | \  /     "
+echo "                   | (_) \__ \ | | | | |_| |_) | |_| /  \     "
+echo "                    \___/|___/_|_| |_|\__|____/ \___/_/\_\    "
+echo " "
 
 # Instalacion de Requisitios
 
 # Instalacion de Herramientas automaticamente
 # dmitry exiftool eyewitness fierce maltego mat2 metagoofil osrframework photon recon-ng sublist3r sherlock
-echo # Installing APPs
-echo # The Following APPs are going to be Installed
-echo # dmitry exiftool eyewitness fierce maltego mat2 metagoofil osrframework photon recon-ng sublist3r sherlock
+echo "# Installing APPs"
+echo "# The Following APPs are going to be Installed"
+echo "# dmitry exiftool eyewitness fierce maltego mat2 metagoofil osrframework photon recon-ng sublist3r sherlock"
 sudo apt install dmitry exiftool eyewitness fierce maltego mat2 mediainfo metagoofil osrframework photon recon-ng sublist3r sherlock -y
-echo # Automated installation of APPs completed
+echo "# Automated installation of APPs completed"
 
-echo # Installing APPs from Github
-echo # The Following APPs are going to be Installed
-echo # OSINTgram Twint TheHarvester Spiderfoot PhoneInfoga
+echo "# Installing APPs from Github"
+echo "# The Following APPs are going to be Installed"
+echo "# OSINTgram Twint TheHarvester Spiderfoot PhoneInfoga"
 
 # OSINTgram
 git clone https://github.com/Datalux/Osintgram.git
@@ -69,38 +69,38 @@ curl -sSL https://raw.githubusercontent.com/sundowndev/phoneinfoga/master/suppor
 ./phoneinfoga version
 sudo mv ./phoneinfoga /usr/bin/phoneinfoga
 
-echo # Installing APPs from Other Sources
-echo # The Following APPs are going to be Installed
-echo # Google Earth Pro
+echo "# Installing APPs from Other Sources"
+echo "# The Following APPs are going to be Installed"
+echo "# Google Earth Pro"
 wget -O google-earth64.deb http://dl.google.com/dl/earth/client/current/google-earth-stable_current_amd64.deb
 sudo dpkg -i google-earth64.deb
 sudo apt-get -f install
 rm google-earth64.deb
 
-echo
-echo                               _       _   ____   _____  __
-echo                      ___  ___(_)_ __ | |_| __ ) / _ \ \/ /
-echo                     / _ \/ __| | '_ \| __|  _ \| | | \  /
-echo                    | (_) \__ \ | | | | |_| |_) | |_| /  \
-echo                     \___/|___/_|_| |_|\__|____/ \___/_/\_\
-echo
-echo #                     All the APPs has been installed.
-echo # 
-echo # Excute the following commands to get info on each tool:
-echo # dmitry -h
-echo # exiftool -h
-echo # eyewitness -h
-echo # fierce -h
-echo # mat2 -h
-echo # metagoofil -h
-echo # sudo osrf -h
-echo # sudo osrf -h
-echo # phoneinfoga -h
-echo # photon -h
-echo # recon-ng -h
-echo # sherlock -h
-echo # sublist3r -h
-echo # theHarvester -h
-echo # sudo twint -h
-echo # 
-echo # 
+echo " "
+echo "                              _       _   ____   _____  __    "
+echo "                     ___  ___(_)_ __ | |_| __ ) / _ \ \/ /    "
+echo "                    / _ \/ __| | '_ \| __|  _ \| | | \  /     "
+echo "                   | (_) \__ \ | | | | |_| |_) | |_| /  \     "
+echo "                    \___/|___/_|_| |_|\__|____/ \___/_/\_\    "
+echo " "
+echo "#                     All the APPs has been installed.        "
+echo "#" 
+echo "# Excute the following commands to get info on each tool:"
+echo "# dmitry -h"
+echo "# exiftool -h"
+echo "# eyewitness -h"
+echo "# fierce -h"
+echo "# mat2 -h"
+echo "# metagoofil -h"
+echo "# sudo osrf -h"
+echo "# sudo osrf -h"
+echo "# phoneinfoga -h"
+echo "# photon -h"
+echo "# recon-ng -h"
+echo "# sherlock -h"
+echo "# sublist3r -h"
+echo "# theHarvester -h"
+echo "# sudo twint -h"
+echo "# "
+echo "# "
