023c8e2e981f769baa2beb47ba679716723aa45f
Dimas <dimas.exe@gmail.com>
2022-04-16 12:41:31 +0200

    Update osintBOX.sh

--- osintBOX.sh
+++ osintBOX.sh
@@ -29,7 +29,6 @@ echo "# The Following APPs are going to be Installed"
 echo "# dmitry exiftool eyewitness fierce maltego mat2 metagoofil osrframework photon recon-ng sublist3r sherlock"
 sudo apt install dmitry exiftool eyewitness fierce maltego mat2 mediainfo metagoofil photon recon-ng sublist3r sherlock -y
 sudo pip3 install osrframework
-sudo pip3 install twint
 
 clear
 echo "#"
@@ -68,6 +67,16 @@ sudo rm v4.0.tar.gz
 cd spiderfoot-4.0
 sudo pip3 install -r requirements.txt
 cd ..
+clear
+
+# Twint
+git clone --depth=1 https://github.com/twintproject/twint.git
+cd twint
+sudo pip install aiohttp==3.7.0
+sudo pip3 install . -r requirements.txt
+sudo pip3 install twint
+cd ..
+sudo rm -r twint/
 clear
 
 # PhoneInfoga
